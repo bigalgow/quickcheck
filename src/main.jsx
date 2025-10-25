36ad6b37da51c8efb47233daace65a6d05b7aceb
Alan G <agow73@gmail.com>
2025-10-25 11:55:28 +0100

    trying hash router for redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import AuthProvider from './auth/AuthProvider.jsx';
 import Callback from './auth/Callback.jsx';
@@ -8,14 +8,13 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        {/* Dedicated callback route for Logto */}
         <Route path="/callback" element={<Callback />} />
-        {/* Your main calculator */}
         <Route path="/*" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>
 );
+
 
